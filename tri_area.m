5a6ee48d647644ef281137e7d39e7a6f7f0d5762
Ipshita Ghosh <iuippi3@gmail.com>
2020-06-07 12:03:23 +0530

    function

---
+++ tri_area.m
@@ -0,0 +1,9 @@
+%{A function to calculate(return) the area of triagle given the base and height as arguments}
+
+function a = tri_area(base,height)
+a = 0.5 * base * height;
+end
+
+
+
+%{TO CALL->  area = tri_area(5,4) }
