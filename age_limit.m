b4365c5ce4e9278901a8df964173573734219481
Ipshita Ghosh <iuippi3@gmail.com>
2020-06-08 21:13:15 +0530

    function with nargin

---
+++ age_limit.m
@@ -0,0 +1,17 @@
+%Write a function called under_age that takes two positive integer scalar arguments:
+%1. age that represents someone's age, and
+%2. limit that represents an age limit.
+%The function returns true if the person is younger than the age limit. If the second argument, limit, is not pro
+%positive integer scalars. The name of the output argument is too_young.
+
+
+function too_young = under_age(age,limit)
+if(nargin==1)
+    limit = 21;
+end
+if(age<limit)
+    too_young = true;
+else
+    too_young = false;
+end
+end
