6b7745d65f883b3fe5a23468461438a34abe3590
Ipshita Ghosh <iuippi3@gmail.com>
2020-06-08 15:51:18 +0530

    matrix advanced

--- matrixadv.m
+++ matrixadv.m
@@ -1,12 +1,12 @@
-%{
-takes a matrix (n*m), and converts in (3*n,m) matrix returns 1st 1/3rd part into 1, 2nd 1/3rd part into 2 and 3rd 1/3rd part into 3 
-     1     1     1     1
-     1     1     1     1
-     2     2     2     2
-     2     2     2     2
-     3     3     3     3
-     3     3     3     3
- }    
+%
+%takes a matrix (n*m), and converts in (3*n,m) matrix returns 1st 1/3rd part into 1, 2nd 1/3rd part into 2 and 3rd 1/3rd part into 3 
+%     1     1     1     1
+%     1     1     1     1
+%     2     2     2     2
+%     2     2     2     2
+%     3     3     3     3
+%     3     3     3     3
+% }    
      
 
 function M = trio(n,m)
