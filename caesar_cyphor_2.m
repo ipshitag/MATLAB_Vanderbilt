69828dda27afd735e222ca0d894653fa40034fea
Ipshita Ghosh <iuippi3@gmail.com>
2020-06-13 13:24:36 +0530

    character array

--- caesar_cyphor_2.m
+++ caesar_cyphor_2.m
@@ -1,3 +1,15 @@
+%{Caesar's cypher is the simplest encryption algorithm.
+It adds a fixed value to the ASCII (unicode) value of each character of a text.
+In other words, it shifts the characters.
+Decrypting a text is simply shifting it back by the same amount, that is, it substract the same value from the characters.
+Write a function called caesar that accepts two arguments: the first is the character vector to be encrypted, 
+%while the second is the shift amount.
+The function returns the output argument coded, the encrypted text.
+The function needs to work with all the visible ASCII characters from space to ~.
+The ASCII codes of these are 32 through 126. If the shifted code goes outside of this range,
+it should wrap around. For example, if we shift ~ by 1, the result should be space. If we shift space by -1, the result should be ~. 
+}
+
 function coded = caesar(M, n)
 num = double(M);                        %Converts string into double
 num2 = num;
