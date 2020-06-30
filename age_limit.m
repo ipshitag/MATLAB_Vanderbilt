837e9f3c87e2bbd7cf5af316070fdd7b2735d05c
Ipshita Ghosh <iuippi3@gmail.com>
2020-06-30 21:03:16 +0530

    Update age_limit.m

--- age_limit.m
+++ age_limit.m
@@ -1,5 +1,6 @@
 %Write a function called under_age that takes two positive integer scalar arguments:
 %1. age that represents someone's age, and
+
 %2. limit that represents an age limit.
 %The function returns true if the person is younger than the age limit. If the second argument, limit, is not pro
 %positive integer scalars. The name of the output argument is too_young.
