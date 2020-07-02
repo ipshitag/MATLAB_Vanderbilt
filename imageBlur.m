f8e9b4bedd9ba90a27127cd34419b0552c04a247
Ipshita Ghosh <iuippi3@gmail.com>
2020-07-02 20:13:08 +0530

    Update imageBlur.m

--- imageBlur.m
+++ imageBlur.m
@@ -1,14 +1,14 @@
 %{Image blur
-
-Write a function called blur that blurs the input image. The function is to be called like this:
-output = blur(img,w);
-where img, the input image is a two-dimensional matrix of grayscale pixel values between 0 and 255. Blurring is to be carried out by averaging the pixel values in the vicinity of
-every pixel. Specifically, the output pixel value is the mean of the pixels in a square submatrix of size 2w+1 where the given pixel sits in the center. For example, if w is 1, then we
-use a 3x3 matrix, that is, we average all the neighboring pixels of the given pixel and itself. Only use valid pixels when portions of the blurring matrix fall outside the image. For
-example, the blurred value corresponding to w = 1 at index (1,1) would be the mean of of elements (1,1), (1, 2), (2,1) and (2, 2). Both input img and output output are of type
-uint8.
-You can download the test image here (https://lcms-files.mathworks.com/content/file/686b475c-bd78-497d-b971-b583832bbf23/vandy.png?
-}
+%
+%Write a function called blur that blurs the input image. The function is to be called like this:
+%output = blur(img,w);
+%where img, the input image is a two-dimensional matrix of grayscale pixel values between 0 and 255. Blurring is to be carried out by averaging the pixel values in the vicinity of
+%every pixel. Specifically, the output pixel value is the mean of the pixels in a square submatrix of size 2w+1 where the given pixel sits in the center. For example, if w is 1, then we
+%use a 3x3 matrix, that is, we average all the neighboring pixels of the given pixel and itself. Only use valid pixels when portions of the blurring matrix fall outside the image. For
+%example, the blurred value corresponding to w = 1 at index (1,1) would be the mean of of elements (1,1), (1, 2), (2,1) and (2, 2). Both input img and output output are of type
+%uint8.
+5You can download the test image here (https://lcms-files.mathworks.com/content/file/686b475c-bd78-497d-b971-b583832bbf23/vandy.png?
+%}
 
 function out = blur(img,w)
     % convert to double for doing calculations
